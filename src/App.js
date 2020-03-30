d57645a66c822f5f3c8b5a96a0e0f1e2bc1f5af8
khoi148 <khoiminhphan@yahoo.com>
2020-03-30 14:59:58 +0700

    cleaned up route syntax

--- src/App.js
+++ src/App.js
@@ -4,16 +4,15 @@ import "bootstrap/dist/css/bootstrap.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Homepage from "./pages/Homepage.js";
 import CreateCandidate from "./pages/CreateCandidate.js";
+import Company from "./pages/Company.js";
+
 export default function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/createcandidate">
-          <CreateCandidate />
-        </Route>
-        <Route path="">
-          <Homepage />
-        </Route>
+        <Route path="/" exact component={Homepage} />
+        <Route path="/candidate" exact component={CreateCandidate} />
+        <Route path="/company" exact component={Company} />
       </Switch>
     </Router>
   );
