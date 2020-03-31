f87124b56e016ef11ad41ed944cdf9481579484e
khoi148 <khoiminhphan@yahoo.com>
2020-03-31 15:47:02 +0700

    took out unnescesarry react Components

--- src/pages/CandidateForm.js
+++ src/pages/CandidateForm.js
@@ -65,158 +65,154 @@ export default function CandidateForm(props) {
   };
   console.log("candidateform ", candidate);
   return (
-    <Container>
-      <Row>
-        <Col>
-          <img src={candidate.photo_url} alt={candidate.first_name} />
-          <Form noValidate validated={validated} onSubmit={onSubmit}>
-            <Form.Row>
-              <Form.Group as={Col} md="4" controlId="validationCustom01">
-                <Form.Label>First name</Form.Label>
-                <Form.Control
-                  required
-                  type="text"
-                  placeholder="First name"
-                  value={candidate.first_name}
-                  onChange={e =>
-                    setCandidate({ ...candidate, first_name: e.target.value })
-                  }
-                />
-                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-                <Form.Control.Feedback type="invalid">
-                  Please provide a first name.
-                </Form.Control.Feedback>
-              </Form.Group>
-              <Form.Group as={Col} md="4" controlId="validationCustom02">
-                <Form.Label>Last name</Form.Label>
-                <Form.Control
-                  required
-                  type="text"
-                  placeholder="Last name"
-                  value={candidate.last_name}
-                  onChange={e =>
-                    setCandidate({ ...candidate, last_name: e.target.value })
-                  }
-                />
-                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-                <Form.Control.Feedback type="invalid">
-                  Please provide a last name.
-                </Form.Control.Feedback>
-              </Form.Group>
-              <Form.Group as={Col} md="4" controlId="validationCustom03">
-                <Form.Label>Email</Form.Label>
-                <InputGroup>
-                  <InputGroup.Prepend>
-                    <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
-                  </InputGroup.Prepend>
-                  <Form.Control
-                    required
-                    type="text"
-                    value={candidate.email}
-                    placeholder="john@email.com"
-                    onChange={e =>
-                      setCandidate({ ...candidate, email: e.target.value })
-                    }
-                  />
-                  <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-                  <Form.Control.Feedback type="invalid">
-                    Please choose a email.
-                  </Form.Control.Feedback>
-                </InputGroup>
-              </Form.Group>
-            </Form.Row>
-            <Form.Row>
-              <Form.Group as={Col} md="6" controlId="validationCustom04">
-                <Form.Label>City</Form.Label>
-                <Form.Control
-                  required
-                  type="text"
-                  placeholder="City"
-                  value={candidate.city}
-                  onChange={e =>
-                    setCandidate({ ...candidate, city: e.target.value })
-                  }
-                />
-                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-                <Form.Control.Feedback type="invalid">
-                  Please provide a valid city.
-                </Form.Control.Feedback>
-              </Form.Group>
+    <div className="container">
+      <img src={candidate.photo_url} alt={candidate.first_name} />
+      <Form noValidate validated={validated} onSubmit={onSubmit}>
+        <Form.Row>
+          <Form.Group as={Col} md="4" controlId="validationCustom01">
+            <Form.Label>First name</Form.Label>
+            <Form.Control
+              required
+              type="text"
+              placeholder="First name"
+              value={candidate.first_name}
+              onChange={e =>
+                setCandidate({ ...candidate, first_name: e.target.value })
+              }
+            />
+            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+            <Form.Control.Feedback type="invalid">
+              Please provide a first name.
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group as={Col} md="4" controlId="validationCustom02">
+            <Form.Label>Last name</Form.Label>
+            <Form.Control
+              required
+              type="text"
+              placeholder="Last name"
+              value={candidate.last_name}
+              onChange={e =>
+                setCandidate({ ...candidate, last_name: e.target.value })
+              }
+            />
+            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+            <Form.Control.Feedback type="invalid">
+              Please provide a last name.
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group as={Col} md="4" controlId="validationCustom03">
+            <Form.Label>Email</Form.Label>
+            <InputGroup>
+              <InputGroup.Prepend>
+                <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
+              </InputGroup.Prepend>
+              <Form.Control
+                required
+                type="text"
+                value={candidate.email}
+                placeholder="john@email.com"
+                onChange={e =>
+                  setCandidate({ ...candidate, email: e.target.value })
+                }
+              />
+              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+              <Form.Control.Feedback type="invalid">
+                Please choose a email.
+              </Form.Control.Feedback>
+            </InputGroup>
+          </Form.Group>
+        </Form.Row>
+        <Form.Row>
+          <Form.Group as={Col} md="6" controlId="validationCustom04">
+            <Form.Label>City</Form.Label>
+            <Form.Control
+              required
+              type="text"
+              placeholder="City"
+              value={candidate.city}
+              onChange={e =>
+                setCandidate({ ...candidate, city: e.target.value })
+              }
+            />
+            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+            <Form.Control.Feedback type="invalid">
+              Please provide a valid city.
+            </Form.Control.Feedback>
+          </Form.Group>
 
-              <Form.Group as={Col} md="3" controlId="validationCustom05">
-                <Form.Label>Country</Form.Label>
-                <Form.Control
-                  required
-                  type="text"
-                  placeholder="U.S.A."
-                  value={candidate.country}
-                  onChange={e =>
-                    setCandidate({ ...candidate, country: e.target.value })
-                  }
-                />
-                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-                <Form.Control.Feedback type="invalid">
-                  Please provide a country.
-                </Form.Control.Feedback>
-              </Form.Group>
-              <Form.Group as={Col} md="3" controlId="validationCustom06">
-                <Form.Label>Photo URL</Form.Label>
-                <Form.Control
-                  required
-                  type="text"
-                  value={candidate.photo_url}
-                  onChange={e =>
-                    setCandidate({
-                      ...candidate,
-                      photo_url: e.target.value
-                    })
-                  }
-                />
-                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-                <Form.Control.Feedback type="invalid">
-                  Please provide a photo URL.
-                </Form.Control.Feedback>
-              </Form.Group>
-            </Form.Row>
-            <Form.Row>
-              <Form.Group as={Col} md="6" controlId="validationCustom07">
-                <Form.Label>Company</Form.Label>
-                <Form.Control
-                  required
-                  type="text"
-                  value={candidate.company}
-                  placeholder="CoderSchool"
-                  onChange={e =>
-                    setCandidate({ ...candidate, company: e.target.value })
-                  }
-                />
-                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-                <Form.Control.Feedback type="invalid">
-                  Please provide a valid company.
-                </Form.Control.Feedback>
-              </Form.Group>
+          <Form.Group as={Col} md="3" controlId="validationCustom05">
+            <Form.Label>Country</Form.Label>
+            <Form.Control
+              required
+              type="text"
+              placeholder="U.S.A."
+              value={candidate.country}
+              onChange={e =>
+                setCandidate({ ...candidate, country: e.target.value })
+              }
+            />
+            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+            <Form.Control.Feedback type="invalid">
+              Please provide a country.
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group as={Col} md="3" controlId="validationCustom06">
+            <Form.Label>Photo URL</Form.Label>
+            <Form.Control
+              required
+              type="text"
+              value={candidate.photo_url}
+              onChange={e =>
+                setCandidate({
+                  ...candidate,
+                  photo_url: e.target.value
+                })
+              }
+            />
+            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+            <Form.Control.Feedback type="invalid">
+              Please provide a photo URL.
+            </Form.Control.Feedback>
+          </Form.Group>
+        </Form.Row>
+        <Form.Row>
+          <Form.Group as={Col} md="6" controlId="validationCustom07">
+            <Form.Label>Company</Form.Label>
+            <Form.Control
+              required
+              type="text"
+              value={candidate.company}
+              placeholder="CoderSchool"
+              onChange={e =>
+                setCandidate({ ...candidate, company: e.target.value })
+              }
+            />
+            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+            <Form.Control.Feedback type="invalid">
+              Please provide a valid company.
+            </Form.Control.Feedback>
+          </Form.Group>
 
-              <Form.Group as={Col} md="3" controlId="validationCustom08">
-                <Form.Label>Job Title</Form.Label>
-                <Form.Control
-                  required
-                  type="text"
-                  placeholder="Developer"
-                  value={candidate.job_title}
-                  onChange={e =>
-                    setCandidate({ ...candidate, job_title: e.target.value })
-                  }
-                />
-                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-                <Form.Control.Feedback type="invalid">
-                  Please provide a valid job title.
-                </Form.Control.Feedback>
-              </Form.Group>
-            </Form.Row>
-            <Button type="submit">Save</Button>
-          </Form>
-        </Col>
-      </Row>
-    </Container>
+          <Form.Group as={Col} md="3" controlId="validationCustom08">
+            <Form.Label>Job Title</Form.Label>
+            <Form.Control
+              required
+              type="text"
+              placeholder="Developer"
+              value={candidate.job_title}
+              onChange={e =>
+                setCandidate({ ...candidate, job_title: e.target.value })
+              }
+            />
+            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+            <Form.Control.Feedback type="invalid">
+              Please provide a valid job title.
+            </Form.Control.Feedback>
+          </Form.Group>
+        </Form.Row>
+        <Button type="submit">Save</Button>
+      </Form>
+    </div>
   );
 }
